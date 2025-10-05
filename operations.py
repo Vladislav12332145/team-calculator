2b724dfb70548e5d0f3d73093e3f29e0a3b6be3c
tyfvute <kurlekstraw@mail.ru>
2025-10-05 16:43:54 +0400

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,13 @@
+def multiply(a, b):
+    """Умножение двух чисел"""
+    return a * b
+
+def divide(a, b):
+    """Деление двух чисел"""
+    if b == 0:
+        return "Ошибка: деление на ноль!"
+    return a / b
+
+def power(a, b):
+    """Возведение в степень"""
+    return a ** b
